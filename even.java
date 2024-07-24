d5311d37ebf1232fa72dcf8647f9dcad933afb5e
Nandhithasadhasivam <166610240+Nandhithasadhasivam@users.noreply.github.com>
2024-07-24 10:04:19 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class even {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        if(num%2==0){
+            System.out.println(num+"Even");
+        }else{
+            System.out.println(num+"Odd");
+        }
+
+    }
+    
+}
+/*56
+56Even*/
