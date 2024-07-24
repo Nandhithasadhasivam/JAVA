8cf7412b2f96eab43628ef98f1c03de7e59c9114
Nandhithasadhasivam <166610240+Nandhithasadhasivam@users.noreply.github.com>
2024-07-24 09:53:49 +0530

    Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        System.out.print("Enter a text:");
+        String s1=s.nextLine();
+        System.out.println(s1);
+        s.close();
+    }
+}
+
+
+/*Enter a text:Hello Github!
+Hello Github!
+*/
